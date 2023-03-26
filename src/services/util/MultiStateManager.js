062067209687595cf72cebe52992cc8fd8bb8d6a
jeromwork <v0vy8l8fcygtmy6mn>
2023-03-26 18:00:57 +0300

    j_feat: add state(name) tu MultiStateManager.js

--- src/services/util/MultiStateManager.js
+++ src/services/util/MultiStateManager.js
@@ -1,34 +1,44 @@
 import { reactive, computed } from 'vue';
 
 export default {
-    _storeName:'default',
+    _stateName:'default',
     setItems(items){
         if(!items)   return this;
-        if(!this._state[this._storeName]) this._state[this._storeName] = {};
+        if(!this._state[this._stateName]) this._state[this._stateName] = {};
         return this._setCashItems(items)._setItemsIds(items);
     },
-    _setCashItems(items){
-        if(!this._state[this._storeName]._cash) this._state[this._storeName]._cash = {};
-        let cashItems = {};
-        for (const item of items) cashItems[item.id] = item;
-        this._state[this._storeName]._cash = { ...this._state[this._storeName]._cash, ...cashItems};
-        return this;
-    },
-    _setItemsIds(items){
-        if(!this._state[this._storeName].itemsIds) this._state[this._storeName].itemsIds = {};
-        let itemsIds = [];
-        for (const item of items) itemsIds.push(item.id);
-        this._state[this._storeName].itemsIds = itemsIds;
-        return this;
-    },
+
     getItems(){
-        const store = this._state[this._storeName];
+        const store = this._state[this._stateName];
         if( !store || !store.itemsIds || !store._cash ) return[];
         let items = [];
         for (const id of store.itemsIds) {
             if( store._cash[id] )items.push(store._cash[id]);
         }
         return items;
-    }
+    },
+
+    state(name){
+        name = (name) ? name : 'default';
+        this._stateName = name;
+        return this;
+    },
+
+    _setCashItems(items){
+        if(!this._state[this._stateName]._cash) this._state[this._stateName]._cash = {};
+        let cashItems = {};
+        for (const item of items) cashItems[item.id] = item;
+        this._state[this._stateName]._cash = { ...this._state[this._stateName]._cash, ...cashItems};
+        return this;
+    },
+
+    _setItemsIds(items){
+        if(!this._state[this._stateName].itemsIds) this._state[this._stateName].itemsIds = {};
+        let itemsIds = [];
+        for (const item of items) itemsIds.push(item.id);
+        this._state[this._stateName].itemsIds = itemsIds;
+        return this;
+    },
+
 
 };
