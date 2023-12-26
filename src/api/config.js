15df16f506d97f41c05b678de40e228278c98223
user <jeromwork@inbox.ru>
2023-12-26 11:15:05 +0300

    j_feat:  set base url is deploy

--- src/api/config.js
+++ src/api/config.js
@@ -1,4 +1,6 @@
 
-            const baseUrl='https://192.168.88.245'
+const isDeploy = process.env.NODE_ENV === 'production'; // Using Node.js environment variable to check if it's in production mode
+
+const baseUrl = isDeploy ? 'https://eastclinic.ru' : 'http://127.0.0.1:8000';
 
 export default baseUrl
