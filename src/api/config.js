b906e0f33fc72d22cb65aa5d057047ef83a5455e
user <jeromwork@inbox.ru>
2023-12-28 14:57:30 +0300

    j_feat: test prod

--- src/api/config.js
+++ src/api/config.js
@@ -1,6 +1,7 @@
 
 const isDeploy = process.env.NODE_ENV === 'production'; // Using Node.js environment variable to check if it's in production mode
 
+console.log(process.env.NODE_ENV)
 const baseUrl = isDeploy ? 'https://eastclinic.ru' : 'http://127.0.0.1:8000';
 const BASE_URL = isDeploy ? 'https://eastclinic.ru' : 'http://127.0.0.1:8000';
 const API_URL = '/api/v1';
